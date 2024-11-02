b19458e77ac946b522372efa4af2322c2736f28e
Dmitry Akatov <dmitry.akatov@protonmail.com>
2024-11-02 12:48:24 +0100

    models

--- src/ec2/ec2.go
+++ src/ec2/ec2.go
@@ -2,6 +2,22 @@ package ec2
 
 import "fmt"
 
+type VPC struct {
+    ID string
+}
+
+type Geo struct {
+    REGION string
+    AVAILABILITY_ZONE string
+    VPC VPC
+}
+
+type EFS struct {
+    ID string
+    NAME string
+    GEO Geo
+}
+
 func Hello(name string) string {
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
     return message
